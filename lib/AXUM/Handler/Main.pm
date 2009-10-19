8025a141d3af057ba3b130ef4dab0680b25426af
Anton Prins <a.prins@d-r.nl>
2009-10-19 14:53:52 +0200

    Initial clean-up for UI website (extracted from Rack)

--- lib/AXUM/Handler/Main.pm
+++ lib/AXUM/Handler/Main.pm
@@ -20,25 +20,6 @@ sub home {
    Tr; th colspan => 2, $self->OEMFullProductName().' configuration'; end;
    Tr; th colspan => 2, 'Global configuration'; end;
    Tr; th $i++; td; a href => '/ipclock', 'IP/Clock configuration'; end; end;
-   Tr; th $i++; td; a href => '/globalconf', 'Global configuration'; end; end;
-   Tr class => 'empty'; th colspan => 2; end; end;
-   Tr; th colspan => 2, 'Buss configuration'; end;
-   Tr; th $i++; td; a href => '/buss', 'Mix buss configuration'; end; end;
-   Tr; th $i++; td; a href => '/monitorbuss', 'Monitor buss configuration'; end; end;
-   Tr class => 'empty'; th colspan => 2; end; end;
-   Tr; th colspan => 2, 'Matrix settings'; end;
-   Tr; th $i++; td; a href => '/source', 'Source configuration'; end; end;
-   Tr; th $i++; td; a href => '/externsrc', 'Extern source configuration'; end; end;
-   Tr; th $i++; td; a href => '/dest', 'Destination configuration'; end; end;
-   Tr; th $i++; td; a href => '/talkback', 'Talkback configuration'; end; end;
-   Tr class => 'empty'; th colspan => 2; end; end;
-   Tr; th colspan => 2, 'Module settings'; end;
-   Tr; th $i++; td; a href => '/module/assign', 'Module assignment'; end; end;
-   Tr; th $i++; td; a href => '/module', 'Module configuration'; end; end;
-   Tr class => 'empty'; th colspan => 2; end; end;
-   Tr; th colspan => 2, 'Hardware settings'; end;
-   Tr; th $i++; td; a href => '/surface', 'Surface configuration'; end; end;
-   Tr; th $i++; td; a href => '/rack', 'Rack configuration'; end; end;
   end;
   $self->htmlFooter;
 }
--- lib/AXUM/Util/HTML.pm
+++ lib/AXUM/Util/HTML.pm
@@ -6,7 +6,7 @@ use warnings;
 use YAWF ':html';
 use Exporter 'import';
 
-our @EXPORT = qw| htmlHeader htmlFooter htmlSourceList OEMFullProductName |;
+our @EXPORT = qw| htmlHeader htmlFooter OEMFullProductName |;
 
 
 sub htmlHeader {
@@ -27,65 +27,6 @@ sub htmlHeader {
      lit " &raquo; ";
      a href => '/', 'Main menu' if $o{page} eq 'home';
      a href => '/ipclock', 'IP/Clock configuration' if $o{page} eq 'ipclock';
-     a href => '/buss', 'Buss configuration' if $o{page} eq 'buss';
-     a href => '/monitorbuss', 'Monitor buss configuration' if $o{page} eq 'monitorbuss';
-     if ($o{page} eq 'source') {
-       a href => '/source', 'Source configuration' if $o{page} eq 'source';
-       if($o{section} eq 'generate') {
-         lit " &raquo; ";
-         a href => "/source/$o{section}", "Generate";
-       }
-       elsif ($o{section})
-       {
-         lit " &raquo; ";
-         a href => "/source/$o{section}/preset", "Source $o{section} preset";
-       }
-     }
-     a href => '/externsrc', 'Extern source configuration' if $o{page} eq 'externsrc';
-     a href => '/dest', 'Destination configuration' if $o{page} eq 'dest';
-     a href => '/talkback', 'Talkback configuration' if $o{page} eq 'talkback';
-     if ($o{page} eq 'module' || $o{page} eq 'modulerouting') {
-       a href => '/module', 'Module configuration';
-       if($o{section}) {
-         lit " &raquo; ";
-         a href => "/module/$o{section}", "Module $o{section}";
-         if($o{page} eq 'modulerouting') {
-           lit " &raquo; ";
-           a href => "/module/$o{section}/route", 'Routing';
-         }
-       }
-     }
-     a href => '/module/assign', 'Module assignment' if $o{page} eq 'moduleassign';
-     a href => '/globalconf', 'Global configuration' if $o{page} eq 'globalconf';
-     if($o{page} eq 'surface') {
-       a href => '/surface', 'Surface configuration';
-       if($o{section}) {
-         lit " &raquo; ";
-         a href => "/surface/$o{section}", "Address $o{section}";
-       }
-     }
-     if($o{page} eq 'rack') {
-       a href => '/rack', 'Rack configuration';
-       if($o{section}) {
-         lit " &raquo; ";
-         a href => "/rack/$o{section}", "Address $o{section}";
-       }
-     }
-     if($o{page} eq 'service') {
-       a href => '/service', 'Service';
-       if ($o{section} eq 'mambanet') {
-         lit " &raquo; ";
-         a href => '/service/mambanet', 'MambaNet configuration';
-       }
-       if($o{section} eq 'predefined') {
-         lit " &raquo; ";
-         a href => '/service/predefined', 'Predefined';
-       }
-       if($o{section} eq 'functions') {
-         lit " &raquo; ";
-         a href => '/service/functions', 'Functions';
-       }
-     }
     end;
     div id => 'content';
 }
@@ -95,26 +36,6 @@ sub htmlFooter {
     end; # /div content
    end; # /body
   end; # /html
-}
-
-
-sub htmlSourceList {
-  my($self, $lst, $name, $min) = @_;
-  div id => $name, class => 'hidden';
-   Select;
-    my $last = '';
-    for (@$lst) {
-      next if $min && $_->{type} eq 'n-1';
-      if($last ne $_->{type}) {
-        end if $last;
-        $last = $_->{type};
-        optgroup label => $last;
-      }
-      option value => $_->{number}, !$_->{active} ? (class => 'off') : (), $_->{label}
-    }
-    end if $last;
-   end;
-  end;
 }
 
 sub OEMFullProductName {
